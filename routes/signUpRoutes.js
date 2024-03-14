830e8ea79b35e45133b1d12dde608f083bddc3b2
kostis <terracominterns2024@gmail.com>
2024-03-14 13:01:27 +0200

    added accounts sign up post request, and notification model

--- routes/signUpRoutes.js
+++ routes/signUpRoutes.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const router = express.Router();
-
+const Account = require('../models/account');
 
 // Define user routes
 router.get('/partner', (req, res) => {
   res.send('GET /sign-up');
 });
 
-router.post('/', (req, res) => {
-  res.send('POST /sign-up');
+router.post('/createAccount', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+
+    const accountData = { ...req.body, password: hashedPassword };
+    const newAccount = await Account.create(accountData);
+    res.status(201).json(newAccount);
+} catch (error) {
+    console.error('Error creating account:', error);
+    res.status(500).send('Error creating account');
+}
 });
 
 // Export the router
