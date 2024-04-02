765a4ffa5cdefa556552626a36c194a42b724b32
kostis <terracominterns2024@gmail.com>
2024-04-02 13:37:12 +0300

    added authMiddleware in order to not serve unauthorized  requests

--- index.js
+++ index.js
@@ -3,21 +3,24 @@ const app = express();
 const PORT = 3000;
 const accountRoutes = require('./routes/accountRoutes');
 const notificationRoutes = require('./routes/notificationRoutes');
-
+const authMiddleware = require('./middleware/authMiddleware');
+const cors = require('cors'); // Import the CORS library
 const cookieParser = require("cookie-parser");
 app.use(express.json());
 
 const db = require('./models');
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
+app.use(cors({
+  origin: 'http://localhost:4200',
+  credentials: true 
+}));
+
+
 
 app.use(cookieParser());
+
+
+app.use(authMiddleware);
 
 app.get('/', (req, res) => {
     res.send('Hello World');
