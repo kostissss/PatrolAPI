b23cd350443cdd1ac9463580487f45002a024218
kostis <terracominterns2024@gmail.com>
2024-04-25 13:09:50 +0300

    added checkPoint model, checkPointRoutes, checkPointsFunctions.
    created basic CRUD functionality for checkPoint model, and added a method to update multiple Checkpoints and a post method to get multiple checkpoints based on a wher clause

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const PORT = 3000;
 const accountRoutes = require('./routes/accountRoutes');
 const notificationRoutes = require('./routes/notificationRoutes');
 const guardRoutes = require('./routes/guardRoutes');
+const checkPointRoutes = require('./routes/checkPointRoutes');
 const authMiddleware = require('./middleware/authMiddleware');
 
 const cors = require('cors'); // Import the CORS library
@@ -23,6 +24,12 @@ app.use(cookieParser());
 
 
 app.use(authMiddleware);
+function loggerMiddleware(req, res, next) {
+  console.log(`${req.method} ${req.path} - ${new Date().toLocaleString()}`);
+  next(); 
+}
+
+app.use(loggerMiddleware); 
 
 
 app.get('/', (req, res) => {
@@ -34,14 +41,18 @@ app.get('/', (req, res) => {
 app.use('/accounts', accountRoutes);
 app.use('/notifications', notificationRoutes);
 app.use('/guards', guardRoutes);
+app.use('/checkPoints', checkPointRoutes);
 
 
-db.sequelize.sync({/* alter: true*/  } ).then(() => {
 
-app.listen(PORT, () => {
-    console.log('Server is running on port 3000');
-    }
 
-);
-});
+db.sequelize
+  .sync({
+    /* alter: true*/
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Server is running on port 3000");
+    });
+  });
 module.exports = app;
---
+++ models/checkPoint.js
@@ -0,0 +1,78 @@
+
+
+module.exports = function(sequelize, DataTypes) {
+    const checkPoint = sequelize.define('checkPoint', {
+        // Model attributes are defined here
+        checkPointId: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+          },
+        checkPointCode: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        clientSiteCode: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+            
+        },
+        ckeckPoint: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            
+        },
+        isLocked: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+            
+        },
+        
+        isDeleted: {
+
+            type: DataTypes.BOOLEAN,
+            allowNull: true,
+            validate: {
+                notEmpty: true
+            }
+            
+        },
+        deletedDate: {
+            type: DataTypes.DATE,
+            allowNull: true,
+            
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+           
+            validate: {
+                notEmpty: true
+            }
+        },
+        
+
+
+     
+       
+    });
+
+    checkPoint.associate = models => {
+        checkPoint.belongsTo(models.Account,{
+            foreignKey: 'userId',
+            
+            allowNull: false,
+            onDelete: 'CASCADE' 
+        });
+    }
+    return checkPoint;
+}
